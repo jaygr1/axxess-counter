4610f2c97d59efb12b147c4f6adb09749b282b73
jaygr1 <jrib6@yahoo.com>
2016-01-20 15:06:44 -0500

    highlighting working

--- js/counter.js
+++ js/counter.js
@@ -1,42 +1,64 @@
-// var interval = setInterval(Counter, 1000);
-
-// function Counter(){
-//   $('#limit').click(function(e){
-//     e.preventDefault();
-//       for (var i = 0; i <= parseInt($('input[type="text"]').val()); i++) {
-//         console.log(i)
-//         // setInterval(function(){
-//           $('#timer').text(i), 1000;
-//         }
-//
-//     })
-//   }
+$( document ).ready(function() {
 
 function Counter(){
-  $('#limit').click(function(e){
-    e.preventDefault();
-    var i = 0;
-    var max = parseInt($('input[type="text"]').val());
-      var callback = function(){
-        i++;
-        $('#timer').text(i);
-        if (i >= max) {
-          clearInterval(x);
-        }
-      }
-      var x = setInterval(callback, 1000);
+ $('#limit').click(function(e){
+   e.preventDefault();
+   var i = 0;
+   var max = parseInt($('input[type="text"]').val());
+     var callback = function(){
+       i++;
+       $('#timer').text(i);
+       FizzBuzz(i);
+       if (i >= max) {
+         clearInterval(x);
+       }
+     }
+     var x = setInterval(callback, 1000);
+   }
+ )}
 
+ function FizzBuzz(num){
+    if (num == 0 || (num % 3 !==0 && num % 5 !== 0)) {
+      $('#toes').removeClass('highlight');
+      $('#fingers').removeClass('highlight');
     }
-  )}
+    else if (num % 3 ===0 && num % 5 === 0) {
+      $('#toes').addClass('highlight');
+      $('#fingers').addClass('highlight');
+    }
+    else if (num % 5 === 0) {
+      $('#toes').addClass('highlight');
+      $('#fingers').removeClass('highlight');
+    }
+    else if (num % 3 === 0) {
+      $('#fingers').addClass('highlight');
+      $('#toes').removeClass('highlight');
+    }
+    else if (num % 3 !== 0) {
+      $('#fingers').removeClass('highlight');
+    }
+    else if (num % 5 !== 0) {
+      $('#toes').removeClass('highlight');
+    }
+  };
 
+  function Reset(){
+   $('#reset').click(function(e){
+     e.preventDefault();
+     debugger;
+        $('#timer').text();
+      })
+   }
 
+ function Restart(){
+  $('#restart').click(function(e){
+    e.preventDefault();
+       $('#timer').text();
+     })
+  }
 
-// function Delay(){
-//   setInterval(function(){
-//     Counter()}),1000;
-// }
+  new Counter()
 
+})
 
-
-
-new Counter()
+// new FizzBuzz()
