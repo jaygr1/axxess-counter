b1ffa1b468050eabd741ba28adc6f161f8c1485d
jaygr1 <jrib6@yahoo.com>
2016-01-21 22:09:40 -0500

    add background

--- js/counter.js
+++ js/counter.js
@@ -1,64 +1,85 @@
+var restart = false;
+var x = null;
+
 $( document ).ready(function() {
 
 function Counter(){
- $('#limit').click(function(e){
-   e.preventDefault();
-   var i = 0;
-   var max = parseInt($('input[type="text"]').val());
-     var callback = function(){
-       i++;
-       $('#timer').text(i);
-       FizzBuzz(i);
-       if (i >= max) {
-         clearInterval(x);
-       }
-     }
-     var x = setInterval(callback, 1000);
+  $('#limit').click(function(e){
+    e.preventDefault();
+    Increment();
+  })
+}
+
+function Increment(){
+  var i = 0;
+  var max = parseInt($('input[type="text"]').val());
+    var callback = function(){
+      i++;
+      // $('#restart').click(function(e){
+      //   e.preventDefault();
+      //   restart = true
+      //   return;
+      // })
+      $('#timer').text(i);
+      FizzBuzz(i);
+      if (i >= max) {
+        clearInterval(x);
+      }
+      // else if (restart == true) {
+      //   return;
+      // }
+    }
+    var x = setInterval(callback, 1000);
+    restart = false;
+}
+
+
+
+function FizzBuzz(num){
+   if (num == 0 || (num % 3 !==0 && num % 5 !== 0)) {
+     $('#toes').removeClass('highlight');
+     $('#fingers').removeClass('highlight');
    }
- )}
-
- function FizzBuzz(num){
-    if (num == 0 || (num % 3 !==0 && num % 5 !== 0)) {
-      $('#toes').removeClass('highlight');
-      $('#fingers').removeClass('highlight');
-    }
-    else if (num % 3 ===0 && num % 5 === 0) {
-      $('#toes').addClass('highlight');
-      $('#fingers').addClass('highlight');
-    }
-    else if (num % 5 === 0) {
-      $('#toes').addClass('highlight');
-      $('#fingers').removeClass('highlight');
-    }
-    else if (num % 3 === 0) {
-      $('#fingers').addClass('highlight');
-      $('#toes').removeClass('highlight');
-    }
-    else if (num % 3 !== 0) {
-      $('#fingers').removeClass('highlight');
-    }
-    else if (num % 5 !== 0) {
-      $('#toes').removeClass('highlight');
-    }
-  };
-
-  function Reset(){
-   $('#reset').click(function(e){
-     e.preventDefault();
-     debugger;
-        $('#timer').text();
-      })
+   else if (num % 3 ===0 && num % 5 === 0) {
+     $('#toes').addClass('highlight');
+     $('#fingers').addClass('highlight');
    }
+   else if (num % 5 === 0) {
+     $('#toes').addClass('highlight');
+     $('#fingers').removeClass('highlight');
+   }
+   else if (num % 3 === 0) {
+     $('#fingers').addClass('highlight');
+     $('#toes').removeClass('highlight');
+   }
+   else if (num % 3 !== 0) {
+     $('#fingers').removeClass('highlight');
+   }
+   else if (num % 5 !== 0) {
+     $('#toes').removeClass('highlight');
+   }
+ };
 
- function Restart(){
-  $('#restart').click(function(e){
+ function Reset(){
+  $('#reset').click(function(e){
     e.preventDefault();
        $('#timer').text();
      })
   }
 
-  new Counter()
+function Restart(){
+ $('#restart').click(function(e){
+   e.preventDefault();
+  //  exit(Increment)
+  //  clearInterval(Increment)
+
+    $('#timer').text(0);
+    Increment();
+ })
+ restart = false
+ }
+
+ new Counter()
+ new Restart()
 
 })
-
-// new FizzBuzz()
